ae227da4197a29ea669612abf58b8da2098ca93d
皮皮强 <pipiqiang@pipiqiang.cn>
2023-03-17 00:38:11 +0800

    fix: 同步代码

--- src/pages/Blog/Article/compoment/artical_menu.tsx
+++ src/pages/Blog/Article/compoment/artical_menu.tsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import { Anchor } from 'antd';
 import { Menu } from '../detail';
+import { useParams } from '@@/exports';
 
-export default class ArticleMenu extends React.Component<any, any> {
-  render(): React.ReactNode {
-    // console.log(this.props);debugger
-    console.log('menu load ......');
+export default function (props: { menus: Array<Menu> }) {
+  const { _to } = useParams();
 
-    const { menus } = this.props;
-    return (
-      <>
-        <Anchor
-          onChange={(s) => console.log(s)}
-          items={menus.map((m: Menu) => {
-            console.log(m);
-            return {
-              href: location.href + '?_to=' + m.name,
-              title: m.name,
-              key: m.name,
-            };
-          })}
-        ></Anchor>
-      </>
-    );
-  }
+  const { menus } = props;
+  return (
+    <>
+      <Anchor
+        onChange={(s) => console.log(s)}
+        getCurrentAnchor={() => _to ?? ''}
+        items={menus.map((m: Menu) => {
+          console.log(m);
+          return {
+            href: location.href + '?_to=' + m.name,
+            title: m.name,
+            key: m.name,
+          };
+        })}
+      />
+    </>
+  );
 }
+
+// export default class ArticleMenu extends React.Component<any, any> {
+//   render(): React.ReactNode {
+//     // console.log(this.props);debugger
+//     console.log('menu load ......');
+//
+//     const { menus } = this.props;
+//     return (
+//
+//     );
+//   }
+// }
